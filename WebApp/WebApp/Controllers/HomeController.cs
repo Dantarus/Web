3b889a9791ca1212d8e124f539c9c8ec67832994
Daniel <danieltyszkiewicz1999@gmail.com>
2020-07-13 15:19:42 +0200

    1.0 version

---
+++ WebApp/WebApp/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //pobieranie danych przy pomocy technologii AdoNet
+            //connectionString dostępny we właściwościach bazy danych
+            //string Login = BaseUserData.GetLogin(2);
+            //string Password = BaseUserData.GetPassword(2);
+            //string ExistInBase = BaseUserData.DataExist(Login, Password);
+            //ViewBag.Login = Login;
+            //ViewBag.Password = Password;
+            //ViewBag.ExistInBase = ExistInBase;
+            return View();
+        }
+        public ActionResult LogIn()
+        {
+
+            return View();
+        }
+        public ActionResult Register()
+        {
+
+            return View();
+        }
+            
+    }
+}
---
+++ WebApp/WebApp/Models/UserDataForm.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApp.Models
+{
+    public class UserDataForm
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string CheckBox { get; set; }
+
+    }
+}
